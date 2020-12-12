9a9f495e99568649dacb0931c4d7606a33443901
InfinityCraftServer <staff.infinitycraft.mc@gmail.com>
2020-12-12 03:00:12 +0100

    Project Setup

---
+++ index.js
@@ -0,0 +1,60 @@
+const Discord = require('discord.js');
+const botsettings = require('./config.json');
+
+const bot = new Discord.Client();
+
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+// register command
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("No commands found");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
+
+// register events (possible)
+fs.readdir("./events/", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("No events found");
+    }
+    jsfile.forEach((f, i) => {
+       require(`./events/${f}`);
+       console.log(`Loading ${f}`)
+    });
+});
+
+
+// commandhandler
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    let prefix = botsettings.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!message.content.startsWith(prefix)) return;
+    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+    if(commandfile) commandfile.run(bot,message,args)
+
+})
+
+bot.login(botsettings.token);
